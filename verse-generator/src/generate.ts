a600cfc31695aa0f65fbd2e45dd4a69e04810e4d
harvv1 <harv256@tutanota.com>
2025-10-27 23:17:53 +0000

    update about me

--- verse-generator/src/generate.ts
+++ verse-generator/src/generate.ts
@@ -1,10 +1,6 @@
 import { verses } from './verses.ts';
 
-console.clear();
-
 export const generate = () => {
 	const values = Object.values(verses);
 	return values[Math.floor(Math.random() * values.length)];
 };
-
-console.log(generate());
