0bc11de6ff9e6addcf88aad3458a9668ce77016c
harvv1 <harv256@tutanota.com>
2025-10-27 19:22:36 +0000

    update

--- harvv1.xyz/src/App.tsx
+++ harvv1.xyz/src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import { generate } from '../../verse-generator/src/generate.ts';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { generate } from '../verse-generator/src/generate.ts';
 import './App.css'
 
 function App() {
   //const [count, setCount] = useState(0)
-  const [message, setMessage] = useState<staring>("");
+  const [message, setMessage] = useState<string>("");
   const handleClick = () =>{
     setMessage(generate())
   }
@@ -15,18 +13,18 @@ function App() {
     <>
       <div>
       <header>
-        <h1 class="glow">Harvey Aaron</h1>
+        <h1 className="glow">Harvey Aaron</h1>
         <p>balla</p>
       </header>
       <section id="about">
-        <h2 class="glow">About Me</h2>
+        <h2 className="glow">About Me</h2>
         <p>ima balla</p>
       </section>
       <section id="projects">
-        <h2 class="glow">Projects</h2>
-        <div class="project">
-            <h3 class="glow">balla projex 1</h3>
-            <button id="myButton" class="glow" onClick={handleClick}>Click me</button>
+        <h2 className="glow">Projects</h2>
+        <div className="project">
+            <h3 className="glow">balla projex 1</h3>
+            <button id="myButton" className="glow" onClick={handleClick}>Click me</button>
             {//<div id="output"></div>
             }
             <p>{message}</p>
